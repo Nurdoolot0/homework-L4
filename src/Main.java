f77377a4f73655ea344abe61fe2941c3bf5edd17
Nurdoolot0 <klopasd34@gmail.com>
2024-07-19 18:37:31 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,128 @@
+import java.util.Random;
+
+public class Main {
+    public static int bossHealth = 700;
+    public static int bossDamage = 50;
+    public static String bossDefence;
+    public static int[] heroesHealth = {260, 270, 250, 200};
+    public static int[] heroesDamage = {20, 15, 10, 0};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medic"};
+    public static int roundNumber = 0;
+
+
+    public static void main(String[] args) {
+        printStatistics();
+        while (!isGameOver()) {
+            playRound();
+        }
+    }
+
+
+    public static boolean isGameOver() {
+        if (bossHealth <= 0) {
+            System.out.println("Heroes won!!!");
+            return true;
+        }
+        /*if (heroesHealth[0] <= 0 && heroesHealth[1] <= 0
+                && heroesHealth[2] <= 0) {
+            System.out.println("Boss won!!!");
+            return true;
+        }
+        return false;*/
+        boolean allHeroesDead = true;
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                allHeroesDead = false;
+                break;
+            }
+        }
+        if (allHeroesDead) {
+            System.out.println("Boss won!!!");
+            return true;
+        }
+        return false;
+    }
+
+    public static void playRound() {
+        roundNumber++;
+        chooseDefence();
+        bossHits();
+        heroesHit();
+        printStatistics();
+        MadicHelp();
+    }
+
+    public static void printStatistics() {
+        System.out.println("ROUND " + roundNumber
+                + " -------------------");
+        /*String defence;
+        if (bossDefence == null) {
+            defence = "No Defence";
+        } else {
+            defence = bossDefence;
+        }*/
+        System.out.println("Boss health: " + bossHealth
+                + " damage: " + bossDamage + " defence: " +
+                (bossDefence == null ? "No Defence" : bossDefence));
+        for (int i = 0; i < heroesHealth.length; i++) {
+            System.out.println(heroesAttackType[i] + " health: "
+                    + heroesHealth[i] + " damage: " + heroesDamage[i]);
+        }
+
+    }
+
+    public static void chooseDefence() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(heroesAttackType.length); // 0,1,2
+        bossDefence = heroesAttackType[randomIndex];
+    }
+
+    public static void bossHits() {
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                if (heroesHealth[i] - bossDamage < 0) {
+                    heroesHealth[i] = 0;
+                } else {
+                    heroesHealth[i] = heroesHealth[i] - bossDamage;
+                }
+            }
+        }
+    }
+
+    public static void MadicHelp() {
+        Random coef = new Random();
+        int randomIndex = coef.nextInt(50);
+        for (int i = 0; i < heroesAttackType.length; i++) {
+            if (heroesAttackType[i] == heroesAttackType[3]) {
+                continue;
+            }
+            if (heroesHealth[i]  > 0 && heroesHealth[i] <100){
+                heroesHealth[i] += randomIndex;
+            }
+            break;
+
+        }
+
+    }
+
+    public static void heroesHit() {
+
+        for (int i = 0; i < heroesDamage.length; i++) {
+            if (heroesHealth[i] > 0 && bossHealth > 0) {
+                int damage = heroesDamage[i];
+                if (bossDefence == heroesAttackType[i]) {
+                    Random random = new Random();
+                    int coeff = random.nextInt(9) + 2; // 2,3,4,5,6,7,8,9,10
+                    damage = heroesDamage[i] * coeff;
+                    System.out.println("Critical damage: ");
+                }
+                if (bossHealth - damage < 0) {
+                    bossHealth = 0;
+                } else {
+                    bossHealth = bossHealth - damage;
+                }
+            }
+        }
+    }
+
+}
